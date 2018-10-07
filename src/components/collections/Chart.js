cc8759cc012e9cae0bb197c03114a0968196c9af
will <will.pankiewicz@gmail.com>
2018-10-07 00:30:30 -0700

    added react-charts-2 graph

---
+++ src/components/collections/Chart.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import {Line, Bar} from 'react-chartjs-2';
+
+import juneJSON from '../../data/2018-06.json'
+
+
+class Chart extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const json = juneJSON;
+    const data = {
+
+  //    Bug in React-chartjs-2 forces label to be put in XAxis
+  //    labels: json.map(x => x['date']),
+      datasets: [{
+        label: 'Steps',
+        type: 'bar',
+        data: json.map(x => x['count']),
+        fill: false,
+        backgroundColor: '#e7aae1',
+        borderColor: '#c584d7',
+        hoverBackgroundColor: '#010515',
+        hoverBorderColor: '#71B37C',
+        yAxisID: 'y-axis-1',
+        lineTension: 0.4
+
+    },{
+      label: 'Calories',
+      type: 'bar',
+      data: json.map(x => x['calorie']),
+      fill: true,
+      backgroundColor: '#010515',
+      borderColor: '#c584d7',
+      hoverBackgroundColor: '#71B37C',
+      hoverBorderColor: '#71B37C',
+      yAxisID: 'y-axis-1'
+
+    },{
+      label: 'Distance',
+      type:'line',
+      data: json.map(x => x['distance']),
+      fill: false,
+      borderColor: '#c584d7',
+      backgroundColor: '#010515',
+      pointBorderColor: '#c584d7',
+      pointBackgroundColor: '#EC932F',
+      pointHoverBackgroundColor: '#EC932F',
+      pointHoverBorderColor: '#EC932F',
+      yAxisID: 'y-axis-2'
+    },
+
+  ]};
+
+  const options = {
+    responsive: true,
+    tooltips: {
+      mode: 'label'
+    },
+    elements: {
+      line: {
+        fill: false
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          display: true,
+          gridLines: {
+            display: false
+          },
+          labels: {
+            show: true
+          },
+          labels: json.map(x => x['date'][10]),
+        }
+      ],
+      yAxes: [
+        {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          id: 'y-axis-1',
+          gridLines: {
+            display: false
+          },
+          labels: {
+            show: true
+          }
+        },
+        {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          id: 'y-axis-2',
+          gridLines: {
+            display: false
+          },
+          labels: {
+            show: true
+          }
+        }
+      ]
+    }
+  };
+
+  const plugins = [{
+      afterDraw: (chartInstance, easing) => {
+          const ctx = chartInstance.chart.ctx;
+          ctx.fillText("Dassha", 100, 100);
+      }
+  }];
+
+    return (
+      <div>
+        <Bar
+          data={data}
+          options={options}
+          plugins={plugins}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chart;
--- src/components/collections/index.js
+++ src/components/collections/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getCollection } from '../../containers/web3/actions';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import LineChart from './Chart';
 
 class Collections extends Component {
   componentDidMount() {
     if (!this.props.collectionsLoaded) this.props.getCollection()
+
   }
 
   render() {
-    return (<div>Collections page</div>)
+    return (
+      <ListGroup>
+        <ListGroupItem>
+          <div className="col-lg-12 col-xs-10 ">
+            <LineChart data={this.props.collections}/>
+          </div>
+        </ListGroupItem>
+      </ListGroup>
+    )
   }
 }
 
